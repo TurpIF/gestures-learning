2ff0e02ba52b152b0080ef4130a07b1802a23894
TurpIF <pierre.turpin@insa-lyon.fr>
2014-04-06 16:09:30 +0200

    Affiche les mouvements trouvés les uns après les autres

--- use_learning.py
+++ use_learning.py
@@ -6,11 +6,16 @@ import time
 from sklearn import neighbors
 
 from learner_mouse import Move
+from learner_mouse import wait_mouse_move
 
 if __name__ == '__main__':
     size = 20
-    move_files = ['assets/left.mv', 'assets/up.mv', 'assets/down.mv',
-            'assets/right.mv', 'assets/null.mv']
+    move_files = ['assets/left.mv', 'assets/right.mv',
+                'assets/up.mv', 'assets/down.mv',
+                'assets/lt_rd.mv', 'assets/rt_ld.mv',
+                'assets/ld_rt.mv', 'assets/rd_lt.mv',
+                'assets/circle.mv',
+                'assets/null.mv']
     moves = []
     for f_name in move_files:
         with open(f_name, 'r') as f:
@@ -21,21 +26,25 @@ if __name__ == '__main__':
     X = map(lambda x: tuple(sum(map(list, x), [])), X)
     Y = map(lambda x: x.name, moves)
 
-    learner = neighbors.KNeighborsClassifier(weights='distance')
+    learner = neighbors.KNeighborsClassifier(
+            n_neighbors=5, weights='distance')
     predictor = learner.fit(X, Y)
 
     mouse = pymouse.PyMouse()
     hist_origin = []
     try:
         while True:
-            pos = mouse.position()
-            hist_origin.append(pos)
-            if len(hist_origin) >= size:
-                o = hist_origin[0]
-                X = map(lambda x: (x[0] - o[0], x[1] - o[1]), hist_origin)
-                X = tuple(sum(map(list, X), []))
-                hist_origin = hist_origin[1:]
-                print predictor.predict(X)[0]
-            time.sleep(0.005)
+            wait_mouse_move()
+            hist_origin = []
+            for _ in xrange(size):
+                pos = mouse.position()
+                hist_origin.append(pos)
+                time.sleep(0.005)
+
+            o = hist_origin[0]
+            X = map(lambda x: (x[0] - o[0], x[1] - o[1]), hist_origin)
+            X = tuple(sum(map(list, X), []))
+            hist_origin = hist_origin[1:]
+            print predictor.predict(X)[0]
     except KeyboardInterrupt:
         pass
